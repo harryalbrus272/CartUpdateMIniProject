7171e21591eb210e073cfc478ea786982f795747
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-04 21:11:43 +0530

    add cart component to make a list of the elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import CartItem from './CartItem';
+import Cart from './Cart';
 function App() {
   const name = 'Shashwat Kumar Singh';
   const isLoggedIn = true;
   return (
     <div className="App">
       <h1>Cart</h1>
-      <CartItem />
+      <Cart />
     </div>
   );
 }
