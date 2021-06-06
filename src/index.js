1e0ebbea37b98ecd62535bf08a7a895c7d080f8f
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-06 18:26:38 +0530

    Hide keys for the firebase

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import firebase from 'firebase';
-import 'firebase/firestore';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase";
+import "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBOrNB78V-98goQXHiXBHHrWglQwt2OS44",
-  authDomain: "cart-57f97.firebaseapp.com",
-  projectId: "cart-57f97",
-  storageBucket: "cart-57f97.appspot.com",
-  messagingSenderId: "121786313210",
-  appId: "1:121786313210:web:1c32315704eb756e2c5c43",
-  measurementId: "G-HT1EVJCV99"
+  apiKey: process.env.REACT_APP_FIRESTORE_API_KEY,
+  authDomain: process.env.REACT_APP_FIRESTORE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIRESTORE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIRESTORE_APP_ID,
+  measurementId: process.env.REACT_APP_FIRESTORE_MEASUREMENT_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -24,7 +24,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
